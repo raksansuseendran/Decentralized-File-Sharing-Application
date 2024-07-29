21db8ac189c7e68307a3c054548e2c8d514724f1
Raksan <98756460+raksansuseendran@users.noreply.github.com>
2024-07-30 03:37:43 +0530

    Add files via upload

---
+++ truffle-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  networks: {
+    loc_development_development: {
+      network_id: "*",
+      port: 8545,
+      host: "127.0.0.1"
+    }
+
+  },
+  mocha: {},
+  compilers: {
+    solc: {
+      version: "0.8.21"
+    }
+  }
+};
